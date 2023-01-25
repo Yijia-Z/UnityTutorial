06c3c55352035dc084837228adfdab7ed74aeff2
Yijia Zhao <zhao.yijia@ufl.edu>
2023-01-25 02:55:59 -0500

    Unity Intro (Part 1)

--- Tutorials/Assets/Exercise 1/Platform.cs
+++ Tutorials/Assets/Exercise 1/Platform.cs
@@ -7,12 +7,16 @@ public class Platform : MonoBehaviour
     public float speed;
     public float distance;
     public float timer;
+    public float direction;//in degrees
+    private float x;
+    private float z;
 
     Vector3 start;
     // Start is called before the first frame update
     void Start()
     {
         //Q1.1) What would happen if we didnt save this? (see Q1.2)
+        //platforms don't start at designated location
         start = gameObject.transform.position;
     }
 
@@ -20,7 +24,22 @@ public class Platform : MonoBehaviour
     void Update()
     {
         //Q1.2) Why would we add start?
-        gameObject.transform.position = start + new Vector3(distance * Mathf.Cos(timer*speed), 0, 0);
+        //to define a relative transform
+        x= Mathf.Cos(direction * Mathf.PI / 180) * distance;
+        z= Mathf.Sin(direction * Mathf.PI / 180) * distance;
+        gameObject.transform.position = start + new Vector3(x * Mathf.Cos(timer*speed), 0, z * Mathf.Cos(timer*speed));
         timer += Time.deltaTime ;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject obj = collision.gameObject;
+        if (obj.tag == "Player")
+            obj.transform.SetParent(this.transform);
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        GameObject obj = collision.gameObject;
+            obj.transform.SetParent(null);
     }
 }
--- Tutorials/Assets/Exercise 1/PlayerController.cs
+++ Tutorials/Assets/Exercise 1/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public float movementSpeed = 1.0f;
+    public float sprintingSpeed = 1.5f;
     public float jumpStrength = 1.0f;
     public float rotationSpeed = 1.0f;
     public float verticalAngleLimit = 85.0f;
 
     private Vector3 currentRotation;
+    private bool enableJump;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -18,6 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         //Grab the rigidbody we want to manipulate for movement
         rb = GetComponent<Rigidbody>();
+        enableJump=true;
     }
 
     // Update is called once per frame
@@ -47,11 +50,15 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(0,0,0);
 
         //Q1) What happens if we dont save this value?
+        //player movement can affect actual velocity of y
         float y = rb.velocity.y;
 
         //For each movement key, update direction of movement
         //Q2) What would happen if the last three if statements were else-if statements?
+        //only the first key pressed gets respond
         //Q3) What would happen if "Camera.main" was replaced with "gameObject"?
+        //gameObject moves in a definite direction
+        
         if (Input.GetKey(KeyCode.W))
         {
             direction += Camera.main.transform.forward;
@@ -71,8 +78,12 @@ public class PlayerController : MonoBehaviour
 
         //Get the normalized vector, then scale based on the current speed
         //Q4) Why do we need to normalize here?
+        //to keep the base velocity as 1.0
         Vector3 velocity = direction.normalized * movementSpeed;
-
+        if (Input.GetKey(KeyCode.F))
+        {
+            velocity*=sprintingSpeed;
+        }
 
         //Add back the y component
         velocity.y = y;
@@ -85,6 +96,11 @@ public class PlayerController : MonoBehaviour
         //When the Space bar is pressed, apply a positive vertical force
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            if (rb.velocity.y==0){
+                enableJump=true;
+            }else if (enableJump){
+                enableJump=false;
+            }else return;
             rb.AddForce(gameObject.transform.up*jumpStrength, ForceMode.Impulse);
         }
     }
