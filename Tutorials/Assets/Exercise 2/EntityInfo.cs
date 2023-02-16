c006a3f4baaafd735058cfb4ca52184e71d67621
Yijia Zhao <zhao.yijia@ufl.edu>
2023-02-16 12:48:56 -0500

    EntityInfo

--- Tutorials/Assets/Exercise 2/EntityInfo.cs
+++ Tutorials/Assets/Exercise 2/EntityInfo.cs
@@ -2,39 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-
 
 public class EntityInfo : MonoBehaviour
 {
-    public bool selected=false;
-    public TMP_Text entityHealth;
-    public TMP_Text entityName;
-    public Material originMaterial , selectedMaterial;
-    // Update is called once per frame
-    void Update()
+    public Text entityNameText;
+    public Slider healthBarSlider;
+    public Color selectedColor = new Color32(255, 150, 35, 200);
+
+    private Entity selectedEntity;
+    public SelectionManager selectionManager;
+    private Color originalColor;
+
+    private void Start()
     {
-        if (selected)
-        {
-            
-        }
+        // Get the original color of the entity
+        originalColor = GetComponent<Renderer>().material.color;
     }
 
-    private void OnMouseDown()
+    private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100))
+        // Check if the selected entity has changed
+        Entity newSelectedEntity = selectionManager.GetSelectedEntity();
+        if (newSelectedEntity != selectedEntity)
         {
-            GameObject go = hit.collider.gameObject;
-            Debug.Log(go.name);
-            if (go == gameObject)
+            // Deselect the previous entity
+            if (selectedEntity != null)
             {
-                selected = true;
-                entityHealth=GameObject.GetComponent<Health>().ToString;
-                return;
+                selectedEntity.GetComponent<Renderer>().material.color = originalColor;
+            }
+            // Select the new entity
+            selectedEntity = newSelectedEntity;
+            if (selectedEntity != null)
+            {
+                // Set the entity's name and health values
+                entityNameText.text = selectedEntity.name;
+                healthBarSlider.value = selectedEntity.Health;
+                // Change the entity's color to show it's selected
+                selectedEntity.GetComponent<Renderer>().material.color = selectedColor;
             }
         }
-        selected=false;
     }
 }
---
+++ Tutorials/Assets/Exercise 2/SelectionManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+    public LayerMask selectableLayer;
+
+    private Entity selectedEntity = null;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, selectableLayer))
+            {
+                Entity entity = hit.collider.GetComponent<Entity>();
+                if (entity != null)
+                {
+                    SelectEntity(entity);
+                }
+            }
+            else
+            {
+                DeselectEntity();
+            }
+        }
+    }
+
+    private void SelectEntity(Entity entity)
+    {
+        selectedEntity = entity;
+    }
+
+    private void DeselectEntity()
+    {
+        selectedEntity = null;
+    }
+
+    public Entity GetSelectedEntity()
+    {
+        return selectedEntity;
+    }
+}
